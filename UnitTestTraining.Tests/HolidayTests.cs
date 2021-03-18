ae2741ba68c20abbef699ca974e006860a31b671
Isaac.Lu <isaac.lu@titansoft.com.sg>
2021-03-18 18:47:57 +0800

    Add unit test project and add two test case

---
+++ UnitTestTraining.Tests/HolidayTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using UnitTestTraining;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestTraining.Tests
+{
+    [TestFixture()]
+    public class HolidayTests //寂寞の小水怪
+    {
+        [Test()]
+        public void Today_Is_XMas()
+        {
+            Assert.AreEqual("Merry X'Mas", new Holiday().SayXMas());
+        }
+
+        [Test()]
+        public void Today_Is_Not_XMas()
+        {
+            Assert.AreEqual("Very sad, Today is not X'Mas", new Holiday().SayXMas());
+        }
+
+    }
+}
