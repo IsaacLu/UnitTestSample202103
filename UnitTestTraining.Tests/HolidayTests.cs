e64605a23e13985e99566ec7679b8889e3cb64a0
Isaac.Lu <isaac.lu@titansoft.com.sg>
2021-03-18 18:56:27 +0800

    Refactor for use same instance to test

--- UnitTestTraining.Tests/HolidayTests.cs
+++ UnitTestTraining.Tests/HolidayTests.cs
@@ -1,25 +1,28 @@
 ﻿using NUnit.Framework;
-using UnitTestTraining;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace UnitTestTraining.Tests
 {
     [TestFixture()]
     public class HolidayTests //寂寞の小水怪
     {
+        private Holiday _holiday;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _holiday = new Holiday();
+        }
+
         [Test()]
         public void Today_Is_XMas()
         {
-            Assert.AreEqual("Merry X'Mas", new Holiday().SayXMas());
+            Assert.AreEqual("Merry X'Mas", _holiday.SayXMas());
         }
 
         [Test()]
         public void Today_Is_Not_XMas()
         {
-            Assert.AreEqual("Very sad, Today is not X'Mas", new Holiday().SayXMas());
+            Assert.AreEqual("Very sad, Today is not X'Mas", _holiday.SayXMas());
         }
-
     }
 }
